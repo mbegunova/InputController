99365e25ec0b8eb3ef10f58832e776d65f559f62
mbegunova <75030702+mbegunova@users.noreply.github.com>
2021-08-05 16:35:22 +0300

    fix(plugin,controller)

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+/***
+ *    ██████╗ ███████╗ ██████╗ ██╗   ██╗██╗██████╗ ███████╗
+ *    ██╔══██╗██╔════╝██╔═══██╗██║   ██║██║██╔══██╗██╔════╝
+ *    ██████╔╝█████╗  ██║   ██║██║   ██║██║██████╔╝█████╗
+ *    ██╔══██╗██╔══╝  ██║▄▄ ██║██║   ██║██║██╔══██╗██╔══╝
+ *    ██║  ██║███████╗╚██████╔╝╚██████╔╝██║██║  ██║███████╗
+ *    ╚═╝  ╚═╝╚══════╝ ╚══▀▀═╝  ╚═════╝ ╚═╝╚═╝  ╚═╝╚══════╝
+ */
+
+const { task, series, parallel } = require("gulp");
+
+
+/***
+ *     ██████╗ ██████╗ ███╗   ██╗███████╗██╗ ██████╗
+ *    ██╔════╝██╔═══██╗████╗  ██║██╔════╝██║██╔════╝
+ *    ██║     ██║   ██║██╔██╗ ██║█████╗  ██║██║  ███╗
+ *    ██║     ██║   ██║██║╚██╗██║██╔══╝  ██║██║   ██║
+ *    ╚██████╗╚██████╔╝██║ ╚████║██║     ██║╚██████╔╝
+ *     ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝     ╚═╝ ╚═════╝
+ */
+const { init } = require("./scripts/gulp/config");
+
+
+/***
+ *    ████████╗ █████╗ ███████╗██╗  ██╗███████╗
+ *    ╚══██╔══╝██╔══██╗██╔════╝██║ ██╔╝██╔════╝
+ *       ██║   ███████║███████╗█████╔╝ ███████╗
+ *       ██║   ██╔══██║╚════██║██╔═██╗ ╚════██║
+ *       ██║   ██║  ██║███████║██║  ██╗███████║
+ *       ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝
+ */
+
+task("build", series(init({ isServe: false }), require("./scripts/gulp/_build")));
+task("serve", series(init({ isServe: true }), require("./scripts/gulp/_serve")));
+task("deploy", require("./scripts/deploy"));
+task("build_dev", series(init({ isServe: false, noVersion: true, isDev: true }), require("./scripts/gulp/_build")));
+
+
+task("styles", require("./scripts/gulp/_styles"));
+task("scripts", require("./scripts/gulp/_scripts"));
+task("pug", require("./scripts/gulp/_pug"));
+
+
+// task("email", require("./scripts/gulp/_email"));
+task("copy-assets", require("./scripts/gulp/_assets").copyAssets);
+task("lint", require("./scripts/gulp/_lint"));
+
+
+task("build_bitrix", series(init({ isBitrix: true }), requireOnDemand("./scripts/gulp/bitrix"), require("./scripts/gulp/_build")));
+
+// task("svg", require("./scripts/gulp/_svg-lib"));
+
+// require('./scripts/gulp/components_status');
+// require('./scripts/gulp/typograf');
+
+function requireOnDemand(path) {
+  return (cb) => {
+    require(path);
+    cb();
+  };
+}
